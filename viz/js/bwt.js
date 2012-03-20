7f446e3219ae3755395bb0d65bfab90d6024638d
Santhosh Kumar J <santhu.j@gmail.com>
2012-03-20 10:49:44 +0530

    Minor improvements. Added place for suffix array and query string

--- viz/js/bwt.js
+++ viz/js/bwt.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
     var suffixes = getSuffixes(text);
 
     bwtViewer = new BWTViewer("bwt");
-    bwtViewer.load(suffixes);
-
+    bwtViewer.load(suffixes.suffixes, suffixes.suffixArray, getRanks(suffixes.suffixes));
+    
+    bwtViewer.search("raca");
 });
 
 /**
@@ -19,7 +20,7 @@ function getSuffixes(text) {
     // Collect all suffixes
     for (var i=0; i<text.length; ++i) {
         var next = text.slice(i, text.length) + text.slice(0, i);
-        suffixes.push(next);
+        suffixes.push({text : next, position : i});
     }
     var sortedSuffixes = suffixes.sort(function(a, b) {
         // Sort logic to use if one the chars is $
@@ -36,9 +37,9 @@ function getSuffixes(text) {
             else
                 return 1;
         };
-        var length = a.length;
+        var length = a.text.length;
         for (var i=0; i < length; ++i) {
-            var ac = a[i], bc = b[i];
+            var ac = a.text[i], bc = b.text[i];
             if (ac === bc)
                 continue;
             if (ac === '$' || bc === '$') {
@@ -49,23 +50,57 @@ function getSuffixes(text) {
         }
         return 0;
     });
-    return sortedSuffixes;
+    var ret = new Array(), suffixArray = new Array();
+    _.each(sortedSuffixes, function(item) {
+        ret.push(item.text);
+        suffixArray.push(item.position);
+    });
+    return {suffixes: ret, suffixArray : suffixArray};
 }
 
+/**
+ * Calculate ranks of alphabets upto the current row
+ */
+function getRanks(suffixes) {
+    var ranks = new Array(), i;
+    var ranksMap = {};
+    var lastColumn = suffixes[0].length - 1;
+    for (i=0; i < suffixes.length; ++i) {
+        ranksMap[suffixes[i][lastColumn]] = 0;
+    }
+    for (i=0; i < suffixes.length; ++i) {
+        ranks.push(deepcopy(ranksMap));
+        ranksMap[suffixes[i][lastColumn]] += 1;
+    }
+    return ranks;
+}
+
+function deepcopy(sourceMap) {
+    var ret = {};
+    for (var i in sourceMap) {
+        ret[i] = sourceMap[i];
+    }
+    return ret;
+}
 function BWTViewer(divID) {
     this.div = $('#' + divID);
     this.paper = Raphael(divID, this.div.width(), this.div.height());
     this.suffixGrid = new SuffixGrid(this.paper);
+    this.query = new Query(this.paper);
 }
 
-BWTViewer.prototype.load = function(suffixes) {
+BWTViewer.prototype.load = function(suffixes, suffixArray, ranks) {
     var size = this.suffixGrid.getSize(suffixes.length, suffixes[0].length);
-    this.div.height(size.height);
+    this.div.height(size.height+50);
     this.div.width(size.width);
-    this.paper.setSize(size.width, size.height);
-    this.suffixGrid.load(suffixes);
+    this.paper.setSize(size.width, size.height+50);
+    this.suffixGrid.load(suffixes, suffixArray, ranks);
+    this.query.load("racab", 10, size.height+10);
 };
 
+BWTViewer.prototype.search = function(query) {
+
+};
 
 function SuffixGrid(paper){
     this.paper = paper;
@@ -73,20 +108,34 @@ function SuffixGrid(paper){
     this.cellPadding = 4;
     this.padding = 10;
     this.cellWidth = 30;
+    this.columnGap = 2; // Gap between suffix array and text grid (in number of columns)
 }
 
-SuffixGrid.prototype.load = function(suffixes) {
-    var i,j;
+SuffixGrid.prototype.load = function(suffixes, suffixArray, ranks) {
+    var i,j,pos,text;
     this.rowCount = suffixes.length;
-    this.colCount = suffixes[0].length;
-    var grid = new Array();
+    this.colCount = suffixes[0].length + 1 + this.columnGap;
+    this.ranks = ranks;
+    
+    this.suffixArray = new Array();
+    // Load suffix array
+    for (i=0; i < this.rowCount; ++i) {
+        pos = this.getTextPostion(i, 0);
+        text = this.paper.text(pos.x, pos.y, suffixArray[i]);
+        text.attr({
+            "font-size" : this.textSize,
+            "text-anchor" : "middle"
+        });
+        this.suffixArray.push(text);
+    }
     // Load the text
+    var grid = new Array();
     for (i=0; i < this.rowCount ; ++i) {
         var row = new Array();
-        for (j=0; j < this.colCount ; ++j) {
+        for (j=0; j < suffixes[0].length; ++j) {
             var next = suffixes[i][j];
-            var pos = this.getTextPostion(i, j);
-            var text = this.paper.text(pos.x, pos.y, next);
+            pos = this.getTextPostion(i, j + 1 + this.columnGap);
+            text = this.paper.text(pos.x, pos.y, next);
             text.attr({
                 "font-size" : this.textSize,
                 "text-anchor" : "middle"
@@ -96,10 +145,12 @@ SuffixGrid.prototype.load = function(suffixes) {
         grid.push(row);
     }
     // Rectangles
+    // Suffix array
+    var suffixArrayColumn = this.drawRectangles(0, "yellow");
     // First column
-    var firstColumn = this.drawRectangles(0, "yellow");
+    var firstColumn = this.drawRectangles((1 + this.columnGap)*this.cellWidth, "green");
     // BWT column (Last column)
-    var xPos = this.cellWidth * (this.rowCount-1);
+    var xPos = this.cellWidth * (this.colCount-1);
     var bwtColumn = this.drawRectangles(xPos, "red");
     var cellHeight = this.getTextItemHeight();
     // Create start and end markers
@@ -157,10 +208,39 @@ SuffixGrid.prototype.getTextItemHeight = function() {
 
 SuffixGrid.prototype.getSize = function(rowCount, colCount) {
     var height = this.getTextItemHeight() * rowCount;
-    var width = this.cellWidth * colCount;
+    var appendedCols = colCount + 1 + this.columnGap; // 1 column for suffix array and 2 for space between
+    var width = this.cellWidth * appendedCols;
     return {
         width : width,
         height : height
     };
 };
 
+function Query(paper) {
+    this.cellWidth = 30;
+    this.cellHeight = 30;
+    this.textSize = 16;
+    this.query = new Array();
+    this.paper = paper;
+    this.rect = paper.rect(0, 0, 0, this.cellHeight).attr({fill : "blue"});
+}
+
+Query.prototype.load = function(query, x, y) {
+    this.rect.attr({x : x, y : y, width: this.cellWidth*query.length});
+    for (var i=0; i < query.length; ++i) {
+        var next = this.paper.rect(x + i*this.cellWidth, y, this.cellWidth, this.cellHeight).attr({fill : 'grey'});
+        this.query.push(next);
+        this.paper.text(x + i*this.cellWidth + this.cellWidth/2, y+this.cellHeight/2, query[i]).attr({
+            "font-size" : this.textSize,
+            "text-anchor" : "middle"
+        });
+    }
+};
+
+Query.prototype.clear = function() {
+    _.each(this.query, function(item) {
+        item.remove();
+    });
+    this.query = new Array();
+    this.rect.attr({width: 0});
+};
